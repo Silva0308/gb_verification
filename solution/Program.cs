015487e5e02846087e1b839cf0b631e2a4d37c19
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-04 21:42:47 +0300

    solution1

---
+++ solution/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string [] ArrayThreeChar (string [] array)
+{
+    string[] newarr = new string[array.Length];
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i].Length == 3)
+        {
+            newarr[i]=array[i];
+        }
+    }
+    return newarr;
+}
+void Print(string [] Array)
+{
+    for (int i=0; i<Array.Length; i++)
+    {
+        Console.WriteLine(Array[i]);
+    }
+    Console.WriteLine();
+}
+string [] new_array = {"hello", "2", "world","123", ":-)"};
+string [] array = ArrayThreeChar (new_array);
+Print (array);
