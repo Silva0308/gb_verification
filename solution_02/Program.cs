78ae79f910284b9fc9017824fdbf9a1a38512ed9
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-05 10:21:57 +0300

    correction solution2

--- solution_02/Program.cs
+++ solution_02/Program.cs
@@ -3,29 +3,40 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-string [] ArrayThreeChar (string [] array)
-{ 
-    string [] result = new string [array.Length];
+string[] ArrayThreeChar(string[] array)
+{
     int realsize = 0;
     foreach (string item in array)
     {
         if (item.Length <= 3)
         {
-            result[realsize] = item;
             realsize++;
         }
     }
+    string[] result = new string[realsize];
+    for (int i = 0; i < realsize; i++)
+    {
+        foreach (string item in array)
+        {
+            if (item.Length <= 3)
+            {
+                result[i] = item;
+                i++;
+            }
+        }
+    }
+
     return result;
 }
 
-void Print(string [] Array)
+void Print(string[] Array)
 {
-    for (int i=0; i<Array.Length; i++)
+    for (int i = 0; i < Array.Length; i++)
     {
         Console.WriteLine(Array[i]);
     }
     Console.WriteLine();
 }
-string [] new_array = {"hello", "2", "world", ":-)","145", "cat"};
-string [] array = ArrayThreeChar (new_array);
-Print (array);
+string[] new_array = { "hello", "2", "world", ":-)", "145", "cat" };
+string[] array = ArrayThreeChar(new_array);
+Print(array);
